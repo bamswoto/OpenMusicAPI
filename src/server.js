ae5b8e698bf7394b2184f27529a78d6f90b03b92
Mohammad Ismail <93242611+moivanismail@users.noreply.github.com>
2022-09-16 12:29:38 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+//import dotenv from 'dotenv';
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/album');
+const AlbumService = require('./services/postgres/AlbumService');
+const AlbumValidator = require('./validator/albums');
+
+const init = async () => {
+    const albumService = new AlbumService();
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register({
+        plugin: albums,
+        options: {
+            service: albumService,
+            validator: AlbumValidator,
+        },
+    });
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
