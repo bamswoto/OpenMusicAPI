475b8a7cbbbdb6b22a0fd122787eef124e85acec
Mohammad Ismail <93242611+moivanismail@users.noreply.github.com>
2022-10-05 21:55:22 +0700

    playlist implemented

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 // albums 
 const albums = require('./api/album');
 const AlbumService = require('./services/postgres/AlbumService');
@@ -21,11 +22,17 @@ const AuthenticationsValidator = require('./validator/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 const TokenManager = require('./tokenize/TokenManager');
 
+// playlist
+const playlists = require('./api/playlists');
+const PlaylistsService = require('./services/postgres/PlaylistsService');
+const PlaylistValidator = require('./validator/playlists');
+
 const init = async () => {
     const albumService = new AlbumService();
     const songService = new SongService();
     const usersService = new UsersService();
     const authenticationsService = new AuthenticationsService();
+    const playlistsService = new PlaylistsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -35,6 +42,28 @@ const init = async () => {
                 origin: ['*'],
             },
         },
+    });
+
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    server.auth.strategy('openmusic_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
 
     await server.register([
@@ -66,6 +95,13 @@ const init = async () => {
                 usersService,
                 tokenManager: TokenManager,
                 validator: AuthenticationsValidator,
+            },
+        },
+        {
+            plugin: playlists,
+            options: {
+                service: playlistsService,
+                validator: PlaylistValidator,
             },
         },
     ]);
--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -1,4 +1,8 @@
+const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
+const InvariantError = require("../../exceptions/InvariantError");
+const NotFoundError = require("../../exceptions/NotFoundError");
+const AuthorizationError = require("../../exceptions/AuthorizationError");
 
 class PlaylistsService {
   constructor() {
@@ -41,7 +45,90 @@ class PlaylistsService {
     const result = await this._pool.query(query);
 
     if (!result.rowCount) {
-      throw new InvariantError('Playlist gagal dihapus. Id tidak ditemukan');
+      throw new NotFoundError('Playlist gagal dihapus. Id tidak ditemukan');
     }
   }
+
+    async verifyPlaylistOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Playlist gagal diverifikasi');
+    }
+
+    const playlist = result.rows[0];
+
+    if (playlist.owner !== owner) {
+      throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+    // async verifyPlaylistAccess(playlistId, userId) {
+    //     try {
+    //         await this.verifyPlaylistOwner(playlistId, userId);
+    //     } catch (error) {
+    //         if (error instanceof NotFoundError) {
+    //             throw error;
+    //         }
+    //         try {
+    //          await this._collaborationService.verifyCollaborator(playlistId, userId);   
+    //         } catch {
+    //             throw error;
+    //         }
+    //     }    
+    // }
+
+    // async addSongToPlaylist(playlistId, songId) {
+    //     const querySong = {
+    //         text: 'select * from songs where id = $1',
+    //         values: [songId],
+    //     };
+
+    //     const result = await this._pool.query(querySong);
+    //     if (!result.rowCount) {
+    //         throw new NotFoundError('Lagu gagal ditambahkan ke playlist. Id lagu tidak ditemukan');
+    //     }
+
+    //     const id = 'playlistsong-' + nanoid(16);
+
+    //     const query = {
+    //         text: 'INSERT INTO playlistsongs VALUES($1, $2, $3) RETURNING id',
+    //         values: [id, playlistId, songId],
+    //     };
+
+    //     await this._pool.query(query);
+
+    //     return result.rows[0].id;
+    // }
+
+    // async getSongsFromPlaylist(playlistId) {
+    //     const playlistQuery = {
+    //         text: 'select playlist.id, playlist.name, users.username from playlists inner join users on playlists.owner = users.id where playlists.id = $1',
+    //         values: [playlistId],
+    //     };
+
+    //     const userQuery = {
+    //         text: 'select users.username as username from users inner join playlists on users.id = playlists.owner where playlists.id = $1',
+    //         values: [playlistId],
+    //     };
+
+    //     const query = {
+    //         text: 'SELECT songs.id, songs.title, songs.performer FROM songs INNER JOIN playlistsongs ON songs.id = playlistsongs.song_id WHERE playlistsongs.playlist_id = $1',
+    //         values: [playlistId],
+    //     };
+
+    //     const result = await this._pool.query(query);
+
+    //     return result.rows;
+
+
+    // }
 }
+
+
+module.exports = PlaylistsService;
